0c7397612ec6cf54d5bedbb37fd7b640480f6c99
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-01-19 09:56:10 +0800

    初始化antd的button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button type="primary">Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
